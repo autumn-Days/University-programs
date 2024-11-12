d2a3a7106ccc6c3615a1dec9e7f0884cc9c342e4
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-11-12 00:23:04 +0000

    .

---
+++ activities/distributedSystems/socketProgrammingBasics/04_P2P_chat/pearOne.py
@@ -0,0 +1,29 @@
+"""
+The "pear one" will be the one that most resembles the server,
+because it will be awainting for a conection request to be mande
+"""
+
+import socket
+
+DISCONNET_MSG = "!DISCONNECT"
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("127.0.0.1", 5051))
+    server.listen()
+    print("[LISTENING]")
+    clientSocket, clientInfo = server.accept()
+    while True:
+
+        msgRcv = (clientSocket.recv(1024)).decode("utf-8")
+        print(msgRcv)
+
+        if msgRcv == DISCONNET_MSG:
+            break
+        
+        msgSend = clientSocket.sendall((input("")).encode())
+
+    server.close()
+
+if __name__ == "__main__":
+    main()
