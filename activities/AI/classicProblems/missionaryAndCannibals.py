a46c0c2caac3b71001eb4f42501240fdab06e810
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-12-01 21:54:57 +0000

    Missionaries and cannibals

--- activities/AI/classicProblems/missionaryAndCannibals.py
+++ activities/AI/classicProblems/missionaryAndCannibals.py
@@ -1,10 +1,17 @@
 from typing import *
+from collections import deque
 
 CANNIBAL_INDEX = 0
 MISSIONARY_INDEX = 1
 
+rule01 = "Rule 01"
+rule02 = "Rule 02"
+rule03 = "Rule 03"
+rule04 = "Rule 04"
+rule05 = "Rule 05"
+
 class Applier:
-    def __init__(self, configuration = [3,3,True]):
+    def __init__(self, configuration = [3,3,True], prev=None, appliedRule = None):
         """
         •There are two sides of the river. The missionaries and the cannibals start at the "initial side"
         of the river and need to go to the "final side" of it, that is, the other side of the river.
@@ -13,6 +20,22 @@ class Applier:
         """
         self.stateFinalSide = [0,0,False]
         self.stateInitialSide = configuration
+        #This is a reference for the previous configuration, so that the path can be traced back
+        self.prev = prev
+        self.appliedRule = appliedRule
+    def solve(self):
+        queue = deque()
+        queue.append(self)
+        while not queue:
+            currentConfig = queue.popleft()
+            if (currentConfig.__isFinished()):
+                currentConfig.__showPath()
+                break
+            queue.append(Applier(currentConfig.__applyRule1(), currentConfig, rule01))
+            queue.append(Applier(currentConfig.__applyRule2(), currentConfig, rule02))
+            queue.append(Applier(currentConfig.__applyRule3(), currentConfig, rule03))
+            queue.append(Applier(currentConfig.__applyRule4(), currentConfig, rule04))
+            queue.append(Applier(currentConfig.__applyRule5(), currentConfig, rule05))
 
     #private
     def __applyRule1(self, amountPassagers = 1, typePassager = CANNIBAL_INDEX):
@@ -58,36 +81,19 @@ class Applier:
     def __isValidConfig(self):
         return ((self.stateInitialSide[1] >= self.stateInitialSide[0]) or (self.stateFinalSide[1] >= self.__updateStateFinalSide[0]))
 
-    def isFinished(self):
+    def __isFinished(self):
         return self.stateFinalSide == [3,3,True]
 
-class Node:
-    def __init__(self):
-        self.currentConfiguration = Applier()
-        children:List['Node'] = None #just like a B-tree
-        parents:List['Node'] = None #just like a B-tree
+    def __showPath(self, currentConfig):
+        listPath = []
+        while currentConfig.appliedRule:
+            listPath.append(currentConfig.appliedRule)
+            curretConfig = currentConfig.prev
+        listPath.reverse()
+        print(listPath)
 
-class Tree:
-    """
-    The general idea is that, for every node, every possible combination
-    will be tested. The 
-    """
-    def __init__(self):
-        self.root = None
-        self.visited:List[str] = None # (0|1|2).(0|1|2).(0|1|2)
-    
-    def BFS(self)
-        return self.__BFS()
-        
-    #private
-    def __BFS (self, parentNode):
-        #generate all the children
-        self.children.append(self.root.currentConfiguration.__applyRule1())
-        self.children.append(self.root.currentConfiguration.__applyRule2())
-        self.children.append(self.root.currentConfiguration.__applyRule3())
-        self.children.append(self.root.currentConfiguration.__applyRule4())
-        self.children.append(self.root.currentConfiguration.__applyRule5())
-        return validPath
+solver = Applier()
+solver.solve()
 
 """
 Some tests:
