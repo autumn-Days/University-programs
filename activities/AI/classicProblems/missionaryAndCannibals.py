a9d909aee9593fe8db01dcf881b8a7d3c9c01d89
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2024-12-02 12:10:44 +0000

    almost finished

--- activities/AI/classicProblems/missionaryAndCannibals.py
+++ activities/AI/classicProblems/missionaryAndCannibals.py
@@ -18,59 +18,62 @@ class Applier:
         •The "state" tells how many cannibals, missionaries are on the side of the river, also the last
         parameter tells whether or not the boat is on that side. 
         """
-        self.stateFinalSide = [0,0,False]
         self.stateInitialSide = configuration
+        self.stateFinalSide = self.__updateStateFinalSide()#[0,0,False]
         #This is a reference for the previous configuration, so that the path can be traced back
         self.prev = prev
         self.appliedRule = appliedRule
     def solve(self):
         queue = deque()
         queue.append(self)
-        while not queue:
+        while queue:
             currentConfig = queue.popleft()
             if (currentConfig.__isFinished()):
                 currentConfig.__showPath()
                 break
-            queue.append(Applier(currentConfig.__applyRule1(), currentConfig, rule01))
-            queue.append(Applier(currentConfig.__applyRule2(), currentConfig, rule02))
-            queue.append(Applier(currentConfig.__applyRule3(), currentConfig, rule03))
-            queue.append(Applier(currentConfig.__applyRule4(), currentConfig, rule04))
-            queue.append(Applier(currentConfig.__applyRule5(), currentConfig, rule05))
+            elif (currentConfig.__isValidConfig()):
+                queue.append(Applier(currentConfig.__applyRule1(), currentConfig, rule01))
+                queue.append(Applier(currentConfig.__applyRule2(), currentConfig, rule02))
+                queue.append(Applier(currentConfig.__applyRule3(), currentConfig, rule03))
+                queue.append(Applier(currentConfig.__applyRule4(), currentConfig, rule04))
+                queue.append(Applier(currentConfig.__applyRule5(), currentConfig, rule05))
 
     #private
     def __applyRule1(self, amountPassagers = 1, typePassager = CANNIBAL_INDEX):
         #Carries one cannibal to the other side of the river
-        if not self.stateInitialSide[2]:
-            self.stateInitialSide[typePassager] += amountPassagers
-            self.stateInitialSide[2] = True
+        stateInitialSide = list(self.stateInitialSide) #passing by copy by using "list"
+        if not stateInitialSide[2]:
+            stateInitialSide[typePassager] += amountPassagers
+            stateInitialSide[2] = True
         else :
-            self.stateInitialSide[typePassager] -= amountPassagers
-            self.stateInitialSide[2] = False
-        self.__updateStateFinalSide()
+            stateInitialSide[typePassager] -= amountPassagers
+            stateInitialSide[2] = False
+        return stateInitialSide
 
     def __applyRule2(self):
         #carries two cannibals to the other side of the river
-        self.__applyRule1(2,CANNIBAL_INDEX)
+        return self.__applyRule1(2,CANNIBAL_INDEX)
 
     def __applyRule3(self):
         #carries one missionary to the other side of the river
-        self.__applyRule1(1, MISSIONARY_INDEX)
+        return self.__applyRule1(1, MISSIONARY_INDEX)
 
     def __applyRule4(self):
         #carries two missionaries to the other side of the river
-        self.__applyRule1(2, MISSIONARY_INDEX)
+        return self.__applyRule1(2, MISSIONARY_INDEX)
     
     def __applyRule5(self):
+        stateInitialSide = self.stateInitialSide
         #carries one missionary and one cannibal to the other side of the river
-        if self.stateFinalSide[2] == 0:
-            self.stateFinalSide[MISSIONARY_INDEX] += 1
-            self.stateFinalSide[CANNIBAL_INDEX] += 1
-            self.stateFinalSide[1] = 1
+        if stateInitialSide[2] == 0:
+            stateInitialSide[MISSIONARY_INDEX] += 1
+            stateInitialSide[CANNIBAL_INDEX] += 1
+            stateInitialSide[2] = True
         else :
-            self.stateFinalSide[MISSIONARY_INDEX] -= 1
-            self.stateFinalSide[CANNIBAL_INDEX] -= 1
-            self.stateFinalSide[1] = 0
-        self.__updateStateFinalSide()
+            stateInitialSide[MISSIONARY_INDEX] -= 1
+            stateInitialSide[CANNIBAL_INDEX] -= 1
+            stateInitialSide[2] = False
+        return stateInitialSide
 
     def __updateStateFinalSide(self):
         a0 = 3 - self.stateInitialSide[0] 
@@ -79,9 +82,11 @@ class Applier:
         self.stateFinalSide = [a0,a1,a2]
 
     def __isValidConfig(self):
-        return ((self.stateInitialSide[1] >= self.stateInitialSide[0]) or (self.stateFinalSide[1] >= self.__updateStateFinalSide[0]))
+        self.__updateStateFinalSide()
+        return ((self.stateInitialSide[1] >= self.stateInitialSide[0]) or (self.stateFinalSide[1] >= self.stateFinalSide[0]))
 
     def __isFinished(self):
+        self.__updateStateFinalSide()
         return self.stateFinalSide == [3,3,True]
 
     def __showPath(self, currentConfig):
