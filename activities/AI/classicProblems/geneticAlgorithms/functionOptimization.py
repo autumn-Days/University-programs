cbfcef9538ad9ad8c016cb8799ebc7de3986f9d9
autumnDays <117274518+autumn-Days@users.noreply.github.com>
2025-01-23 13:45:14 +0000

    almost done

--- activities/AI/classicProblems/geneticAlgorithms/functionOptimization.py
+++ activities/AI/classicProblems/geneticAlgorithms/functionOptimization.py
@@ -15,11 +15,53 @@ For this problem it will be no mutations.
 """
 
 import math
+import random
+import heapq
 from typing import *
 
-def generateFirstPopulation():
+def generateFirstPopulation() -> List[int]:
     population:List[int] = []
-
+    cromossome = ""
+    while len(population) != 4:
+        for gene in range (4):
+            cromossome += str(random.randint(0,1))
+        population.append(int(cromossome,2))
+        cromossome = ""
+    return population
 
 def function2optimize(cromossome:int):
     return math.sin(int(cromossome,2))
+
+def crossOver(parent01:int, parent02:int) -> int:
+    fstHalfP1 = str(parent01)[:2] 
+    sndHalfP2 = str(parent02)[2:]
+    child = fstHalfP1 + sndHalfP2
+    return int(child,2)
+
+
+def getGoodParents(population:List[int]) -> List[Tuple[int,int]]:
+    cromossomesFeatness:List[Tuple[int,int]] = []
+
+    for cromossome in population :
+        featness = function2optimize(cromossome)
+        cromossomesFeatness.append((featness,cromossome))
+
+    heapq.heapify(cromossomesFeatness)
+
+    parent01 = -heap.heappop(cromossomesFeatness)
+    parent02 = -heap.heappop(cromossomesFeatness)
+
+    return parent01, parent02
+
+def __genNewPopulation(parent01:int, parent02:int, population) -> List[int]:
+    if len(population == 4):
+        return population
+    
+    
+
+def genNewPopulation(parent01:int, parent02:int) -> List[int]:
+    return __genNewPopulation(parent01, parent02, [])
+
+print(generateFirstPopulation())
+
+print(crossOver(int("1111",2),int("1111",2)))
